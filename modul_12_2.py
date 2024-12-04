58195690e569528aa458b0e05f23353db58bd9d5
IoanMamsurov <ivan.mamsurov@yandex.ru>
2024-12-04 22:20:45 +0300

    Update modul_12_2.py

--- modul_12_2.py
+++ modul_12_2.py
@@ -16,23 +16,24 @@ class TournamentTest(unittest.TestCase):
 
     def test_tournament_1(self):
         one = m_12_2.Tournament(90, self.first, self.third)
-        one.start()
-        self.setUpClass()
+        self.all_results[1] = one.start()
+
 
     def test_tournament_2(self):
         two = m_12_2.Tournament(90, self.second, self.third)
-        two.start()
-        self.setUpClass()
+        self.all_results[2] = two.start()
 
     def test_tournament_3(self):
         three = m_12_2.Tournament(90, self.first, self.second, self.third)
-        three.start()
-        self.setUpClass()
+        self.all_results[3] = three.start()
+
+
+
 
     @classmethod
     def tearDownClass(cls):
-        for key in cls.all_results:
-            print(key, cls.all_results[key])
+        for key, value in cls.all_results.items():
+            print(value)
 
 
 if __name__ == '__main__':
